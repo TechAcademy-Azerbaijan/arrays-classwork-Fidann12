55a56cf559b0a0c079c7ea6b2f9691e521186c13
Fidann12 <110629295+Fidann12@users.noreply.github.com>
2022-08-30 11:56:44 +0400

    Update main.js

--- task-5/main.js
+++ task-5/main.js
@@ -5,8 +5,10 @@ prompt.start();
 prompt.get('input', function (err, result) {
   
     let n = result.input.split(" ")
-    let arr = ['n'];
     let b = n[n.length -1]
     n.pop ();
     n.unshift(b)
-    console.log(n);
+    console.log(n.join(" "));
+
+  
+});
